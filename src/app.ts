28b7d33681ffe5d3590aba61d16b1bdb7da3c81a
Hardeep-Malhotra <hardeepmalhotra022@gmail.com>
2026-04-03 21:33:52 +0530

    feat: migrate to ES modules and set up Express server
    - Changed package type to module in package.json for ES module support.
    - Added Express as a dependency and @types/express for TypeScript support.
    - Created a new app.ts file to define the Express application and a basic route.
    - Updated server.ts to import the app and start the server on a specified port.
    - Modified tsconfig.json for improved module resolution and TypeScript settings.

---
+++ src/app.ts
@@ -0,0 +1,17 @@
+import express from 'express';
+
+
+const app = express();
+
+
+
+
+// Routes 
+
+app.get('/' ,(req,res) =>{
+    res.send('Welcome to the Book API');
+})
+
+
+
+export default app;
