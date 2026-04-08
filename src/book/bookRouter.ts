a305b1d91fd29b843c909abc5f5a7034bbd1493a
Hardeep-Malhotra <hardeepmalhotra022@gmail.com>
2026-04-08 14:28:36 +0530

    feat: implement file upload functionality in book router

--- src/book/bookRouter.ts
+++ src/book/bookRouter.ts
@@ -1,10 +1,25 @@
 import express from 'express';
 import { createBook } from '../book/bookController.js';
-
+import multer from 'multer';
+import path from 'node:path';
 const bookRouter = express.Router();
+
+const upload = multer({
+  dest: path.resolve(__dirname, '../../public/data/uploads'),
+  limits: {
+    fileSize: 3e7,
+  },
+});
 
 // Book router
 
-bookRouter.post('/', createBook);
+bookRouter.post(
+  '/',
+  upload.fields([
+    { name: 'coverImage', maxCount: 1 },
+    { name: 'file', maxCount: 1 },
+  ]),
+  createBook
+);
 
 export default bookRouter;
